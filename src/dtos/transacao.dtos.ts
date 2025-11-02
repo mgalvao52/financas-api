638b26c2b3e0a5fe220dca5b13672adcb4cb74f6
Marco Galvao <marco.galvao@farec.sp.gov.br>
2025-11-02 13:49:32 -0300

    Correção add transação

--- src/dtos/transacao.dtos.ts
+++ src/dtos/transacao.dtos.ts
@@ -4,7 +4,7 @@ export const transacaoSchema = z.object({
     descricao: z.string().min(3,'descrição deve ter pelo menos 3 caracteres'),
     valor: z.number().positive('valor deve ser positivo'),
     tipo: z.enum(['entrada','saida']),
-    usuarioId: z.number().int(),
+    usuarioId: z.number().int().optional(),
     contaId:number().int(),
     categoriaId:number().int(),
     data:z.date().optional()
